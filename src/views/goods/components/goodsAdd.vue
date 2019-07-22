501ddceb0b59c712fa6f6b2440be181dc0e2cbef
dd <89@qq.com>
2019-07-22 20:27:59 +0800

    商品图片完成

--- src/views/goods/components/goodsAdd.vue
+++ src/views/goods/components/goodsAdd.vue
@@ -18,9 +18,21 @@
     <el-tab-pane name="1" label="基本属性">基本属性</el-tab-pane>
     <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
     <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
-    <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
+    <el-tab-pane name="4" label="商品图片">
+     <el-upload
+  class="upload-demo"
+  action="https://jsonplaceholder.typicode.com/posts/"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :file-list="fileList"
+  list-type="picture">
+  <el-button size="small" type="primary">点击上传</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+</el-upload>
+    </el-tab-pane>
     <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
   </el-tabs>
+</el-upload>
 </div>
 </template>
 
@@ -29,10 +41,23 @@
       name:"goodsAdd",
     data() {
       return {
-        active: "1"  //数字转字符串，双引号
+        active: "1" , //数字转字符串，双引号
+        fileList:[]
+      };
+    }, 
+ methods: {
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+      },
+      handlePreview(file) {
+        console.log(file);
       }
     }
-    };
+  };
+    
+    
+    
+  
 
    
 </script>
