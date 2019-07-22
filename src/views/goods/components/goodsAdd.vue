0b709dd5383b4fea735ac0494f6552ed15b83cfa
dd <89@qq.com>
2019-07-22 22:15:21 +0800

    富文本编辑完成

--- src/views/goods/components/goodsAdd.vue
+++ src/views/goods/components/goodsAdd.vue
@@ -13,39 +13,61 @@
   <el-step title="商品属性"></el-step> 
   <el-step title="商品图片"></el-step>
   <el-step title="商品内容"></el-step>
-</el-steps>
+ </el-steps>
   <el-tabs v-model='active' tab-position="left" style="height: 200px;">
     <el-tab-pane name="1" label="基本属性">基本属性</el-tab-pane>
     <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
     <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
     <el-tab-pane name="4" label="商品图片">
+     <!-- 此处地址并没有让图片传入服务器，必须要改成基地址才行 -->
      <el-upload
-  class="upload-demo"
-  action="https://jsonplaceholder.typicode.com/posts/"
-  :on-preview="handlePreview"
-  :on-remove="handleRemove"
-  :file-list="fileList"
-  list-type="picture">
+       class="upload-demo"   
+      action="http://localhost:8888/api/private/v1/upload" 
+      :headers="sendHeaders"   
+      :file-list="fileList"
+       list-type="picture"
+       :on-success='success'
+  >
   <el-button size="small" type="primary">点击上传</el-button>
   <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
 </el-upload>
     </el-tab-pane>
-    <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+    <el-tab-pane name="5" label="商品内容">
+     <quillEditor></quillEditor> 
+     </el-tab-pane>
   </el-tabs>
 </el-upload>
 </div>
 </template>
 
 <script>
+// require styles  导入样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+//导入富文本组件
+import { quillEditor } from 'vue-quill-editor'
+
   export default {
       name:"goodsAdd",
+      //局部注册组件
+       components:{
+        quillEditor
+      },
     data() {
       return {
         active: "1" , //数字转字符串，双引号
-        fileList:[]
+        fileList:[],
+        sendHeaders:{
+          //设置token
+          Authorization:window.sessionStorage.getItem('token')
+        }
       };
     }, 
  methods: {
+        success(){
+          console.log('成功了');
+        },
       handleRemove(file, fileList) {
         console.log(file, fileList);
       },
